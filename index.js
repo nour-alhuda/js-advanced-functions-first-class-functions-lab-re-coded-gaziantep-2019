9c51a2075676f491f84ce60e6111c6250278c80a
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-08-18 21:33:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrDrivers){
+  return arrDrivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(arrDrivers){
+  return drivers.slice(drivers.length-2, drivers.length);
+}
